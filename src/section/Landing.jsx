318ef0934cde4d0ec75bfe1531f234ed1789cca2
Bhumir2503 <36862986+Bhumir2503@users.noreply.github.com>
2024-02-05 11:02:09 -0500

    landing Page

--- src/section/Landing.jsx
+++ src/section/Landing.jsx
@@ -5,14 +5,23 @@ function Landing() {
     <div className="landing">
       <div className='left'>
         <div className='meInfo'>
-          <h1></h1>
+          <h1>Bhumir Patel</h1>
+          <h3>Full Stack Developer</h3>
+          <p>
+            My journey as a full stack developer began in high school, where my fascination with technology ignited. 
+            It all started with a simple curiosity, tinkering with code in my spare time, exploring the intricacies of both front-end and back-end development. 
+            From those early days of building basic websites and experimenting with different programming languages, my passion only grew stronger. 
+            As I delved deeper into the world of software development, I honed my skills, constantly seeking new challenges and opportunities to expand my knowledge.
+          </p>
         </div>
         <div className='download'>
           <button>Download<img src="download.svg" alt="" /></button>
         </div>
       </div>
       <div className='right'>
-        
+        <div className='landimg'>
+          <img src="animation.svg" alt="" />
+        </div>
       </div>
     </div>
   );
