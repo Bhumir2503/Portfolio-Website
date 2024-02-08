2c80f2af3e42e4e9d1e878ec4333806f4e47ed17
Bhumir2503 <36862986+Bhumir2503@users.noreply.github.com>
2024-02-07 21:20:14 -0500

    Logos

--- src/section/Project.jsx
+++ src/section/Project.jsx
@@ -4,7 +4,73 @@ function Project() {
 
 
   return (
-    <></>
+    <div className="Project">
+      <div className="left">
+        <h1>Projects</h1>
+      </div>
+      <div className="right">
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+      </div>
+    </div>
   );
 }
 
